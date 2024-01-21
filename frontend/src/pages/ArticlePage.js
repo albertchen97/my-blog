a803e200b00ff7c7071762d6c20a6551c91d500c
Haoze Chen <albertchen2017@outlook.com>
2024-01-20 16:53:14 -0800

    Add upvote display to the article page

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,16 +1,37 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import OtherArticles from "../components/OtherArticles";
 import articles from "./article-content";
+
 const ArticlePage = () => {
 	const { articleId } = useParams();
+	const [articleInfo, setArticleInfo] = useState({
+		upvotes: 0,
+		comments: [],
+	});
+
+	// Use the useEffect() hook to synchronize the the ArticlePage component's state with the server data.
+	useEffect(() => {
+		const loadArticleInfo = async () => {
+			const response = await axios.get(`/api/articles/${articleId}`);
+			const newArticleInfo = response.data;
+
+			setArticleInfo(newArticleInfo);
+		};
+		loadArticleInfo();
+	}, [articleId]);
+
 	const article = articles.find((article) => article.name === articleId);
+
 	if (!article) {
 		return <NotFoundPage />;
 	}
 	return (
 		<>
 			<h1>{article.title}</h1>
+			<p>This article has {articleInfo.upvotes} upvote(s).</p>
 			{article.content.map((paragraph, i) => (
 				<p key={i}>{paragraph}</p>
 			))}
