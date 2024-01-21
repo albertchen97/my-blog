d2119884ee97ab9cb3addb5a0e012b4349c1444b
Haoze Chen <albertchen2017@outlook.com>
2024-01-21 10:19:58 -0800

    Add CommentList component to ArticlePage

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -4,20 +4,20 @@ import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import OtherArticles from "../components/OtherArticles";
 import articles from "./article-content";
+import CommentsList from "../components/CommentsList";
 
 const ArticlePage = () => {
-	const { articleId } = useParams();
 	const [articleInfo, setArticleInfo] = useState({
 		upvotes: 0,
 		comments: [],
 	});
+	const { articleId } = useParams();
 
 	// Use the useEffect() hook to synchronize the the ArticlePage component's state with the server data.
 	useEffect(() => {
 		const loadArticleInfo = async () => {
 			const response = await axios.get(`/api/articles/${articleId}`);
 			const newArticleInfo = response.data;
-
 			setArticleInfo(newArticleInfo);
 		};
 		loadArticleInfo();
@@ -31,11 +31,27 @@ const ArticlePage = () => {
 	return (
 		<>
 			<h1>{article.title}</h1>
-			<p>This article has {articleInfo.upvotes} upvote(s).</p>
+			<p className="upvotes">
+				This article has <b>{articleInfo.upvotes}</b> upvote(s).
+			</p>
 			{article.content.map((paragraph, i) => (
 				<p key={i}>{paragraph}</p>
 			))}
-			<h2>Check out my other articles:</h2>
+
+			<h2>Comments:</h2>
+			{/* {articleinfo.comments?.map((comment) => (
+				<div
+					classname="comment"
+					key={comment.postedby + ": " + comment.text}
+				>
+					<h4>{comment.postedby}</h4>
+					<p>{comment.text}</p>
+				</div>
+			))} */}
+			<CommentsList comments={articleInfo.comments} />
+
+			{/* todo: put otherarticles to the right of the article content */}
+			<h2>check out my other articles:</h2>
 			<OtherArticles currentArticle={article} />
 		</>
 	);
