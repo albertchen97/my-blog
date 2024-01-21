84ecd8fc918c284486117e85383abc2623bfa2f6
Haoze Chen <albertchen2017@outlook.com>
2024-01-21 10:42:06 -0800

    Update ArticleList styling: Add a border under the article titles and make the border width fit the title length.

--- frontend/src/components/OtherArticles.js
+++ frontend/src/components/OtherArticles.js
@@ -12,12 +12,11 @@ const OtherArticles = ({ currentArticle }) => {
 				<Link
 					key={article.name}
 					to={`/articles/${article.name}`}
-					// className="article-list-item"
+					className="article-list-item"
 				>
 					<div className="article-list-item-wrapper">
 						<div className="otherarticle-list-item">
 							<p>{article.title}</p>
-							{/* <p>{article.content[0].substring(0, 150)}...</p> */}
 						</div>
 					</div>
 				</Link>
